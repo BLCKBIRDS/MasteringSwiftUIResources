2203772e3e16e610c5e477ddbc2ace8adff6fa7a
BLCKBIRDS <andreasschultz@Andreass-MBP.fritz.box>
2019-12-27 00:05:18 +0100

    Grouped files + hide API key

--- Chapter 13 - City Explorer/CityExplorer/CityExplorer/Helper/APIHelper.swift
+++ Chapter 13 - City Explorer/CityExplorer/CityExplorer/Helper/APIHelper.swift
@@ -6,7 +6,7 @@
 
 import Foundation
 
-let apiKey = "28d1aee79b65f7ad5cae06c64f153d2b"
+//let apiKey = *insert your apiKey as a String"
 
 func generateFlickrURL(latitude: Double, longitude: Double, numberOfPhotos: Int) -> String {
     let url = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&lat=\(latitude)&lon=\(longitude)&radius=1&radius_units=km&per_page=\(numberOfPhotos)&format=json&nojsoncallback=1"
